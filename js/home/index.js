99b3a65f6ae2819827566d6c12932877b835cc10
daPeng <1192721032@qq.com>
2017-04-05 21:46:08 +0800

    登录功能完成

--- js/home/index.js
+++ js/home/index.js
@@ -1,3 +1,8 @@
-define([], function() {
-	
+define(['jquery'], function ($) {
+  $.post('v6/login', {
+    tc_name: '前端学院',
+    tc_pass: '123456'
+  }, function (data) {
+    console.log(data);
+  })
 });
--- js/home/login.js
+++ js/home/login.js
@@ -1,3 +1,25 @@
-define([], function() {
-	
+define(['jquery'], function($) {
+
+    //监听表单提交事件并阻止，转而实现ajax请求
+	$('#login-form').on('submit',function(){
+        
+        //this是form原生对象，对form进行包装，才能调用jq的方法
+        console.log($(this).serialize()); 
+        console.log($(this).serializeArray());
+                
+        $.ajax({
+            url:'/v6/login',
+            type:'POST',
+            data:$(this).serialize(),
+            success:function(data){
+                if(data.code == 200){
+                    location.href='/';                
+                }
+            },
+            error:function(){
+               alert(arguments[2]);
+            }
+        })
+        return false;
+    })
 });
