666957a17670a55bf112ba7561af40902562412b
daPeng <1192721032@qq.com>
2017-04-06 22:05:06 +0800

    焦点/ajax全局loading/讲师列表渲染

--- js/home/index.js
+++ js/home/index.js
@@ -1,5 +1,4 @@
-define(['jquery','common','header','aside','nprogress'], 
-function ($,undefined,undefined,undefined,nprogress) {
-  // 页面所有代码执行完毕，进度条结束
-  nprogress.done();
+define(['jquery', 'common', 'header', 'aside', 'nprogress'], function($, undefined, undefined, undefined, nprogress) {
+	// 页面所有代码执行完毕，进度条结束
+	nprogress.done();
 });
