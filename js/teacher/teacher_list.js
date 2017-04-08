47ed2ec4788b70911adc38d887fa6fbdff1f5031
daPeng <1192721032@qq.com>
2017-04-08 14:11:49 +0800

    teacher功能添加，编辑

--- js/teacher/teacher_list.js
+++ js/teacher/teacher_list.js
@@ -1,4 +1,4 @@
-define(['aside', 'header', 'loading', 'nprogress', 'jquery', 'template'], function (undefined, undefined, undefined, nprogress, $, template) {
+define(['bootstrap', 'aside', 'header', 'loading', 'nprogress', 'jquery', 'template'], function (undefined, undefined, undefined, undefined, nprogress, $, template) {
 
     // 渲染讲师列表
     (function () {
@@ -7,11 +7,54 @@ define(['aside', 'header', 'loading', 'nprogress', 'jquery', 'template'], functi
 		 * 2、请求成功后调用template方法得到讲师列表html
 		 * 3、把html添加到页面中即可
 		 * */
-        $.get('/v6/teacher', function (data) 
-        {
+        $.get('/v6/teacher', function (data) {
             if (data.code === 200) {
                 $('.table-teacher-list').append(template('teacher-list-tpl', data));
             }
+        });
+    })();
+
+    //渲染讲师详情
+    //动态创建的dom元素最好使用事件委托来绑定事件
+    (function () {
+		/**
+		 * 1、发送请求获取讲师列表
+		 * 2、请求成功后调用template方法得到讲师列表html
+		 * 3、把html添加到页面中即可
+		 * */
+        $(document).on('click', '.teacher-view', function () {
+            $.get('/v6/teacher/view', {
+                tc_id: $(this).data('teacher-id')
+            }, function (data) {
+                if (data.code === 200) {
+                    $('#teacher-modal').html(template('teacher-view-tpl', data.result));
+                }
+            });
+        });
+    })();
+
+    // 注销启动讲师
+    (function () {
+        $(document).on('click', '.teacher-handle', function () {
+            var self = this;
+            $.post('/v6/teacher/handle', {
+                tc_id: $(this).data('teacher-id'),
+                tc_status: $(this).data('teacher-status')
+            }, function (data) {
+                if (data.code == 200) {
+
+                    // 如果为0，讲师当前是开启状态，那么更新按钮的自定义属性为当前状态，
+                    // 但是按钮显式相反的状态：注销，因为按钮表示的是点击后用户会成为什么状态。
+                    // 如果为1则相反。
+                    if (data.result.tc_status == 0) {
+                        $(self).data('teacher-status', 0);
+                        $(self).html('注销');
+                    } else {
+                        $(self).data('teacher-status', 1);
+                        $(self).html('启用');
+                    }
+                }
+            });
         });
     })();
 
